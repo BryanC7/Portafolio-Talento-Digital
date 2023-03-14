d7cb20f543df7397ba04db640279b3dc331159fe
Bryan Cabrera <bryancabreram3@gmail.com>
2023-03-14 19:56:42 -0300

    Creación carpeta public para archivos img y css. Cambios menores en archivos js

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const app = express()
 const hbs = require("hbs")
+
 app.set('view engine', 'hbs');
 app.set('views', './views');
 app.use(express.static("public"))
-hbs.registerPartials(__dirname+"/views/partials")
+hbs.registerPartials(__dirname + "/views/partials")
 
 app.listen(3000)
-
-app.get("/",(req, res)=>{
-    res.render("index")
-})
 
 app.get("/templates",(req, res)=>{
     res.render("templates")
