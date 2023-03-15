039f4e269409b0de473ae20ba3d61fdda8c35d8b
Bryan <bryancabreram3@gmail.com>
2023-03-15 13:31:34 -0300

    Cambios menores en archivo js y json

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,21 @@
-const express = require("express");
+const express = require('express')
 const app = express()
-const hbs = require("hbs")
+const hbs = require('hbs')
 
-app.set('view engine', 'hbs');
-app.set('views', './views');
-app.use(express.static("public"))
-hbs.registerPartials(__dirname + "/views/partials")
+app.set('view engine', 'hbs')
+app.set('views', './views')
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'))
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'))
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'))
+app.use(express.static(__dirname + 'public'))
+hbs.registerPartials(__dirname + '/views/partials')
 
 app.listen(3000)
 
-app.get("/templates",(req, res)=>{
-    res.render("templates")
+app.get('/index',(req, res)=>{
+    res.render('index')
+})
+
+app.get('/templates',(req, res)=>{
+    res.render('templates')
 })
